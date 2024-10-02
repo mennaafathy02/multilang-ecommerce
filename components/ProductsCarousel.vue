13e4599acdb8813ded332d9103d2ff9fda80c60b
mennaafathy02 <mennaafathy1014@gmail.com>
2024-10-02 16:46:09 +0300

    category page

--- components/ProductsCarousel.vue
+++ components/ProductsCarousel.vue
@@ -13,7 +13,7 @@ const { t, locale } = useI18n()
 defineProps({
     category: {
         type: String,
-        required: true,
+        required: false,
     },
     products: {
 
@@ -36,16 +36,17 @@ defineProps({
 
         </h2>
         <Carousel>
-            <CarouselContent class="-ml-2 gap-4 md:-ml-4">
+            <CarouselContent class="-ml-2 md:-ml-4">
                 <CarouselItem v-for="(item, index) in products" :key="index"
-                    class="pl-2  overflow-hidden md:pl-4 sm:basis-1/2 lg:basis-1/4">
+                    class="overflow-hidden md:pl-4 sm:basis-1/2 md:basis-1/4 lg:basis-1/5">
                     <ProductCard :item="item" />
                 </CarouselItem>
             </CarouselContent>
             <CarouselPrevious />
             <CarouselNext />
         </Carousel>
-        <Button class="block my-20 mx-auto" @click="navigateTo(`/products/${category}`)">
+        <Button v-if="!$route.name?.toString().includes('category')" class="block my-20 mx-auto"
+            @click="navigateTo(`/products/${category}`)">
             {{ t('view_all_products') }}
         </Button>
     </div>
--- pages/products/category/[slug].vue
+++ pages/products/category/[slug].vue
@@ -1,4 +1,19 @@
+<script setup lang="ts">
+import type { ProductItem } from '~/types/types';
+
+const route = useRoute()
+
+const { data: categoryProducts } = useFetch<Record<string, ProductItem> | null>(`https://fakestoreapi.com/products/category/${route.params.slug}`)
+// Convert the Record to an array and handle the null case
+const productsArray = computed(() => {
+    return categoryProducts.value ? Object.values(categoryProducts.value) : [];
+});
+
+</script>
 <template>
-    dsds
+    <div class="px-10 font-bold text-xl capitalize mb-8">
+        {{ $route.params.slug }}
+    </div>
+    <ProductsCarousel :products="productsArray" />
 
 </template>
